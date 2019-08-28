113292c3791777bc21a80004b0b8538c223d9249
Ronan Lambert <rflambert@live.co.uk>
2019-08-28 19:11:10 +1200

    Add project files.

---
+++ SkipAmmonomiconAnimation/SkipAmmonomiconAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkipAmmonomiconAnimation
+{
+    public class SkipAmmonomiconAnimation : ETGModule
+    {
+        public override void Init()
+        {
+        }
+
+        public override void Start()
+        {
+            ETGModConsole.Log("SkipAmmonomiconAnimation - Initialisation Successful");
+        }
+
+        public override void Exit()
+        {
+        }
+    }
+}
